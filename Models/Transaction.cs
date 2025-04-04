ec09d754e33de68fdfc29fb851dcadc4819e47a8
damini.s@finology.in <damini.s@finology.in>
2025-04-04 12:56:03 +0530

    creating model

---
+++ Models/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTrackerMVC.Models
+{
+	public class Transaction
+	{
+		[Key]
+		public int TransactionId { get; set; }
+
+		public int CategotyId { get; set; }
+		public Category Category { get; set; } // nevigational proprties 
+		public int Amount { get; set; }
+		[Column(TypeName = "nvarchar(75)")]
+		public string? Note { get; set; }
+		public DateTime Date { get; set; } = DateTime.Now;
+	}
+}
